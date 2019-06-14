d31daa46cb63807e36d5a1130b97da1e1f33f386
Monish <monish.ramadoss@outlook.com>
2019-06-14 03:13:46 -0700

    update

--- vkFlow/vkFlow.cpp
+++ vkFlow/vkFlow.cpp
@@ -1,37 +1,18 @@
-#include <Python.h>
+#include <pybind11/pybind11.h>
 #include <vector>
 #include "documentaion.h"
 #include "pipeline/pipeline.h"
 
+
+namespace py = pybind11;
 /*
  * Implements an example function.
  */
 
 
-PyObject* set_device(PyObject* self, PyObject* args, PyObject* kwargs) {
-	PyObject* obj = NULL;
-	int number = 0;
-
-	static char* keywords[] = {"deviceNum", NULL };
-	if (!PyArg_ParseTupleAndKeywords(args, kwargs, "i", keywords, &obj, &number)) {
-		return NULL;
-	}
-	if (number < 0) {
-		PyErr_SetObject(PyExc_ValueError, obj);
-		return NULL;    /* return NULL indicates error */
-	}
-	int deviceProc = PY_UINT32_T(obj);
-	Py_RETURN_NONE;
-}
-
-PyObject* Run(PyObject* self, PyObject* args, PyObject* kwargs) {
-	
-	PyObject* obj = NULL;
-	int number = 0;
-
+void Run() {
 	auto y = std::vector<float>(128, 1.0f);
 	auto x = std::vector<float>(128, 2.0f);
-
 
 	pipeline::Instance instance = pipeline::Instance();
 	pipeline::Device device = instance.devices().at(0);
@@ -40,62 +21,15 @@ PyObject* Run(PyObject* self, PyObject* args, PyObject* kwargs) {
 	pipeline::Array<float> device_y = pipeline::Array<float>(device, x);
 
 	using Spec = pipeline::typelist<uint32_t>;
-	struct Params {uint32_t size, float a};
+	struct Params { uint32_t size; float a; };
 
-	auto program = pipeline::Program<Specs, Params>(device, "shader");
-	program.grid(128 / 64).spec(64)({ 128, 0.1 }, d_y, d_x);
+	auto program = pipeline::Program<Spec, Params>(device, "shader");
+	program.grid(128 / 64).spec(64)({ 128, 0.1 }, device_y, device_x);
 
-	d_y.toHost(begin(y));
-
-
-	Py_RETURN_NONE;
+	device_y.toHost(begin(y));
 }
 
-/*
- * List of functions to add to vkFlow in exec_vkFlow().
- */
-static PyMethodDef vkFlow_functions[] = {
-    { "Run", (PyCFunction)Run, METH_NOARGS, vkFlow_Run_doc },
-	{ "set_device", (PyCFunction)set_device, METH_VARARGS | METH_KEYWORDS, vkFlow_example_doc },
-    { NULL, NULL, 0, NULL } /* marks end of array */
-};
-
-/*
- * Initialize vkFlow. May be called multiple times, so avoid
- * using static state.
- */
-int exec_vkFlow(PyObject *module) {
-    PyModule_AddFunctions(module, vkFlow_functions);
-    PyModule_AddStringConstant(module, "__author__", "monish");
-    PyModule_AddStringConstant(module, "__version__", "1.0.0");
-
-
-    return 0; /* success */
-}
-
-/*
- * Documentation for vkFlow.
- */
-PyDoc_STRVAR(vkFlow_doc, "The vkFlow module");
-
-
-static PyModuleDef_Slot vkFlow_slots[] = {
-    { Py_mod_exec, exec_vkFlow }, { 0, NULL }
-};
-
-static PyModuleDef vkFlow_def = {
-    PyModuleDef_HEAD_INIT,
-    "vkFlow",
-    vkFlow_doc,
-    0,              /* m_size */
-    NULL,           /* m_methods */
-    vkFlow_slots,
-    NULL,           /* m_traverse */
-    NULL,           /* m_clear */
-    NULL,           /* m_free */
-};
-
-PyMODINIT_FUNC PyInit_vkFlow() {
-	
-    return PyModuleDef_Init(&vkFlow_def);
+PYBIND11_MODULE(vkFlow, m) {
+	m.doc() = "Run Pipeline";
+	m.def("Run()", &Run, "Run Model",)
 }
