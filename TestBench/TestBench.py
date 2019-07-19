7ee081b1f2d33802822f43750545f4dd2aa9eac4
Ramadoss <monish.ramadoss@outlook.com>
2019-07-18 17:01:03 -0700

    onnx parser

--- TestBench/TestBench.py
+++ TestBench/TestBench.py
@@ -1,11 +1,30 @@
 import numpy as np
 from PIL import Image
-import vkFlow as vkFlow
+import google.protobuf as protobuf
+from onnx import ModelProto, GraphProto, NodeProto
+
+##import vkFlow as vkFlow
 import onnx
+def o_g():
+    onnx_model = onnx.load('mobilenetv2.onnx')
+    graph = onnx_model.graph
+    init_vals = graph.initializer
+    for val, node in zip(graph.initializer, graph.node):
+        dim = val.dims
+        float_data = val.float_data
+        layer = val.name
+        print(type(node))
+        print(type(val))
+    inputs = []
+    outputs = []
+    
+    
+       
+    return inputs
+
 
 if (__name__ == "__main__"):
-    onnx_model = onnx.load('mobilenetv2.onnx')
+    vals = o_g()
     img = Image.open("aerial.png")
     x = np.array(img)
-    print(x.shape)
    # vkFlow.Run()
--- vkFlow/layer_repo.h
+++ vkFlow/layer_repo.h
@@ -1,7 +1,7 @@
 #ifndef LAYERREPO_H
 #define LAYERREOP_H
-#include "onnx/onnx_pb.h"
-#include "onnx/proto_utils.h"
+//#include "onnx/onnx_pb.h"
+//#include "onnx/proto_utils.h"
 
 #include <vector>
 
