72c2de91cbfc678f8c2522aa795568524b263401
unknown <monish.ramadoss@outlook.com>
2019-10-01 00:10:18 -0700

    bug fixes

--- onnx_builder/compile_shader.py
+++
@@ -1,5 +0,0 @@
-import os
-import subprocess
-for name in os.listdir("../_backend/shaders"):
-    if(os.path.isfile("../_backend/shaders/"+name)):
-        subprocess.call("glslangValidator -V -o ../_backend/shaders/bin/"+ name.split('.')[0]+'.spv' + " ../_backend/shaders/"+name)
